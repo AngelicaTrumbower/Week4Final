d9ba1b667e485b40a1bfd0ecdf42b54439caad0b
AngelicaTrumbower <a_m_hello@yahoo.com>
2024-07-24 21:01:48 -0400

    added sort filter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const searchForm = document.getElementById("input__form");
+const filter = document.getElementById("filter");
 
 
 document.getElementById('input__form').addEventListener('submit', function (event) {
@@ -18,21 +19,39 @@ async function fetchMovies(event) {
     const movieData = await movies.json();
 
     const moviesArray = movieData.Search;
+    console.log(moviesArray);
+
     for(let i = 0; i < 9; i++) {
+        console.log(moviesArray[i].Year)
         if (moviesArray[i]) {
             const movieContainer = document.getElementById(`movie${i + 1}`);
             if (movieContainer) {
                 movieContainer.innerHTML = displayMovies(moviesArray[i]);
-            } else {
-                console.error(`Element with id 'movie${i + 1}' not found or no data available.`)
-            }
+            };
         };
     };
-    const movieContainer = document.querySelector('.movie__container');
-}
+    if (filter === "Title:A-Z") {
+        (moviesArray[i].Title).sort((a, b) => (a.moviesArray[i].Title) - (b.moviesArray[i].Title));
+    }
+    else if (filter === "Year: New to Old") {
+        (moviesArray[i].Year).sort((a, b) => (a.moviesArray[i].Year) - (b.moviesArray[i].Year));
+    }
+    else if (filter === "Year: Old to New") {
+        (moviesArray[i].Year).sort((a, b) => (b.moviesArray[i].Year) - (a.moviesArray[i].Year));
+    };
+};
 
 function displayMovies(movie) {
-    return `<img src="${movie.Poster}">
-            <p>Title: ${movie.Title}</p>
+    return `<img src= ${movie.Poster}>
+            <p>Title: "${movie.Title}"</p>
             <p>Year: ${movie.Year}</p>`
-}
+};
+
+function filterMovies(event) {
+    fetchMovies(event.target.value);
+    console.log(fetchMovies(event.target.value));
+};
+
+setTimeout(() => {
+    fetchMovies();
+}, 2000);
